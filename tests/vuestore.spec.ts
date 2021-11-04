2afe76aad2da1634ffacb82160d4944acb8b71a4
Pierce Corcoran <pierce.corcoran@dfstudio.com>
2021-11-04 13:16:15 -0700

    Add unit tests

---
+++ tests/vuestore.spec.ts
@@ -0,0 +1,131 @@
+import chai, {assert, expect} from 'chai';
+import spies from 'chai-spies';
+import VueStore from '../src';
+import Vue, {nextTick, watch} from "vue";
+
+chai.use(spies)
+
+type C = { new(...args: any[]): {} }
+
+function testStores(storeFunction: <T extends C>(constructor: T) => T) {
+  it("properties should be preserved", () => {
+    @storeFunction
+    class Store {
+      plain = 10
+      'quotes!' = 20
+      declared: number
+
+      constructor() {
+        this.declared = 30
+        this['notDeclared'] = 40
+      }
+    }
+
+    let store = new Store()
+    expect(store).to.include({plain: 10, 'quotes!': 20, declared: 30, notDeclared: 40})
+  });
+
+  it("properties should be reactive", async () => {
+    @storeFunction
+    class Store {
+      plain = 10
+      declared: number
+
+      constructor() {
+        this.declared = 20
+        this['notDeclared'] = 30
+      }
+    }
+
+    let store = new Store()
+    store['late'] = 40
+
+    let plainSpy = chai.spy()
+    let declaredSpy = chai.spy()
+    let notDeclaredSpy = chai.spy()
+    let lateSpy = chai.spy()
+    watch(() => store.plain, plainSpy)
+    watch(() => store.declared, declaredSpy)
+    watch(() => store['notDeclared'], notDeclaredSpy)
+    watch(() => store['late'], lateSpy)
+
+    store.plain = 100
+    store.declared = 200
+    store['notDeclared'] = 300
+    store['late'] = 400
+
+    await nextTick()
+
+    expect(plainSpy).to.be.called.with(100, 10)
+    expect(declaredSpy).to.be.called.with(200, 20)
+    expect(notDeclaredSpy).to.be.called.with(300, 30)
+    expect(lateSpy).to.be.called.with(400, 40)
+  });
+
+  it("watches should trigger", async () => {
+    @storeFunction
+    class Store {
+      plain = 10
+
+      constructor(
+          private __spies: { plainSpy(...args) }
+      ) {
+      }
+
+      'on:plain'(...args) {
+        this.__spies.plainSpy(...args)
+      }
+    }
+
+    let plainSpy = chai.spy()
+    let store = new Store({plainSpy})
+
+    store.plain = 100
+
+    await nextTick()
+
+    expect(plainSpy).to.be.called.with(100, 10)
+  });
+
+  it("should not hold references", function(done) {
+    this.timeout(5000)
+
+    @storeFunction
+    class Store {
+      constructor(public prop: number) {}
+    }
+
+    let blackhole: (Store | null)[] = [null, null]
+    let baseline = process.memoryUsage().heapUsed
+    let didCollect = false // whether a net-negative GC pass was executed
+    for (let i = 0; i < 20; i++) {
+      let start = process.memoryUsage().heapUsed
+      for(let j = 0; j < 10000; j++) {
+        blackhole[j % 2] = new Store(j)
+      }
+      let end = process.memoryUsage().heapUsed
+      if(end < start) {
+        didCollect = true
+        break
+      }
+    }
+    if(!didCollect) {
+      assert.fail('a net-negative GC pass should run when creating 200,000 stores')
+    }
+    done()
+  })
+}
+
+
+describe("@VueStore", () => {
+  testStores(VueStore)
+});
+
+describe("VueStore.create", () => {
+  testStores(<T extends C>(constructor: T) => {
+        return function (...args: any[]) {
+          return VueStore.create(new (constructor as C)(...args))
+        } as unknown as T
+      }
+  )
+});
